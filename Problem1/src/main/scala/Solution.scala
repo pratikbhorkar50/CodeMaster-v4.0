b099b24bc3aed9979dff57c353597eb566d59e8d
Aaron Rumao <aaron-pascoal.rumao@worldline.com>
2021-08-23 12:20:41 +0530

    attempt commit

--- Problem1/src/main/scala/Solution.scala
+++ Problem1/src/main/scala/Solution.scala
@@ -7,22 +7,23 @@ object Solution extends App {
   val input = Source.fromResource("input.txt").getLines()
   val writer = new PrintWriter(new File("output.txt"))
 
-  var remainingLines = input.drop(1)
-  var count = 0
+  var remainingLines = input.drop(1).toList
+  var count = 1
   while (remainingLines.nonEmpty) {
-    val linesToTake = remainingLines.next().split(" ").map(_.trim).last.toInt + 1
-    doSomething(count, remainingLines.take(linesToTake).toList)
+    val linesToTake = remainingLines.head.split(" ").map(_.trim).last.toInt + 1
+    doSomething(count, remainingLines.take(linesToTake))
     remainingLines = remainingLines.drop(linesToTake)
     count = count + 1
   }
 
-  def doSomething(count: Int, lines: List[String]) = {
+  def doSomething(count: Int, lines: List[String]): Unit = {
     val totalKM = lines.head.split(" ").map(_.trim).head.toInt
     val blah = lines.drop(1).map(line => {
       val somethings = line.split(" ").map(_.trim)
-      (totalKM - somethings.head.toInt) / somethings.last.toInt
+      val sol = (totalKM - somethings.head.toInt) / somethings.last.toInt
+      sol
     }).sum
-    val calc = totalKM / blah
+    val calc = totalKM.toDouble / blah.toDouble
     writer.write(s"Trip-$count: $calc\n")
   }
   writer.close()
