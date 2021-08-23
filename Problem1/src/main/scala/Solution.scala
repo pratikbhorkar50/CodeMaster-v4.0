d658076635594b05a8f3867b09675236e2bef687
Aaron Rumao <aaron-pascoal.rumao@worldline.com>
2021-08-23 11:54:08 +0530

    some changes

--- Problem1/src/main/scala/Solution.scala
+++ Problem1/src/main/scala/Solution.scala
@@ -7,5 +7,23 @@ object Solution extends App {
   val input = Source.fromResource("input.txt").getLines()
   val writer = new PrintWriter(new File("output.txt"))
 
+  var remainingLines = input.drop(1)
+  var count = 0
+  while (remainingLines.nonEmpty) {
+    val linesToTake = remainingLines.next().split(" ").map(_.trim).last.toInt + 1
+    doSomething(count, remainingLines.take(linesToTake).toList)
+    remainingLines = remainingLines.drop(linesToTake)
+    count = count + 1
+  }
+
+  def doSomething(count: Int, lines: List[String]) = {
+    val totalKM = lines.head.split(" ").map(_.trim).head.toInt
+    val blah = lines.drop(1).map(line => {
+      val somethings = line.split(" ").map(_.trim)
+      (totalKM - somethings.head.toInt) / somethings.last.toInt
+    }).sum
+    val calc = totalKM / blah
+    writer.write(s"Trip-$count: $calc\n")
+  }
   writer.close()
 }
--- SOLUTION-README.md
+++ SOLUTION-README.md
@@ -1,6 +1,6 @@
-We have attempted Problem[1/2]
+We have attempted Problem[1]
 
-Solution can be found at Problem[1/2]/src/main/scala/Solution.scala
+Solution can be found at Problem[1]/src/main/scala/Solution.scala
 
-Input at : Problem[1/2]/src/main/resources/input.txt
-Output at : Problem[1/2]/output.txt
+Input at : Problem[1]/src/main/resources/input.txt
+Output at : Problem[1]/output.txt
