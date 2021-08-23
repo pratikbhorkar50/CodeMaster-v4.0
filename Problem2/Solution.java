a81a642d58f418825febdf990176118e2da88d0a
arulrajmichael <arulraj.michael@hotmail.com>
2021-08-23 12:37:08 +0530

    final codemaster commit
    updated the Solution.java with Working solution and Problem2_output.txt with Final output

--- Problem2/Solution.java
+++ Problem2/Solution.java
@@ -4,33 +4,93 @@ public class Solution{
 	String inputLine;
 	boolean luckyNumber;
 	
+	public String dToC(int count, int num) {
+		String temp = "";
+		for (int i = 0; i < count; i++) {
+			temp = temp + num;
+		}
+		return temp;
+	}
+	
+	public String getNumbers(String s) {
+
+		int zero = 0;
+		int one = 0;
+		int two = 0;
+		int three = 0;
+		int four = 0;
+		int five = 0;
+		int six = 0;
+		int seven = 0;
+		int eight = 0;
+		int nine = 0;
+
+		int n = s.length();
+
+		for (int i = 0; i < n; i++) {
+			if (s.charAt(i) == 'Z')
+				zero++;
+			if (s.charAt(i) == 'O')
+				one++;
+			if (s.charAt(i) == 'W')
+				two++;
+			if (s.charAt(i) == 'H')
+				three++;
+			if (s.charAt(i) == 'U')
+				four++;
+			if (s.charAt(i) == 'V')
+				five++;
+			if (s.charAt(i) == 'X')
+				six++;
+			if (s.charAt(i) == 'S')
+				seven++;
+			if (s.charAt(i) == 'G')
+				eight++;
+			if (s.charAt(i) == 'N')
+				nine++;
+		}
+
+		one = one - (two + four + zero);
+		three = three - eight;
+		seven = seven - six;
+		five = five - seven;
+		nine = (nine - (one + seven)) / 2;
+
+		String finalString = dToC(zero, 0) + dToC(one, 1) + dToC(two, 2) + dToC(three, 3) + dToC(four, 4)
+				+ dToC(five, 5) + dToC(six, 6) + dToC(seven, 7) + dToC(eight, 8) + dToC(nine, 9);
+		return finalString;
+	}
+
+	
 	private void read() throws IOException{
 		try{
-			//FileReader fr = new FileReader("C:/Users/a676508/workspace/devWorks/CM4.0/src/Problem2_input2.txt");
+			//FileReader fr = new FileReader("C:/Users/a676508/workspace/devWorks/CM4.0/src/Problem2_input.txt");
 			FileReader fr = new FileReader("Problem2_input.txt");
 			BufferedReader br = new BufferedReader(fr);
-			//File file = new File("C:/Users/a676508/workspace/devWorks/CM4.0/src/output.txt");
+			//File file = new File("C:/Users/a676508/workspace/devWorks/CM4.0/src/Problem2_output.txt");
 			File file = new File("Problem2_output.txt");
 			FileWriter fw = new FileWriter(file);
 			BufferedWriter bw = new BufferedWriter(fw);
 			int count = 0;
 			int totalRooms =0;
 			int roomCounter =0;
-			
+			String strOp = "";
 			
 			while((inputLine = br.readLine())!=null){
 				String[] dataArr = inputLine.split(" ");
-				System.out.println(inputLine+":"+dataArr[0]);
-				
 				if(count==0){
 					totalRooms = Integer.parseInt(dataArr[0]);
 					count++;
 					continue;
 				}else{
 					roomCounter++;
+				// strOp = getNumbers(inputLine);
+					bw.write("Room-:"+roomCounter+":"+getNumbers(inputLine));
+					System.out.println("Room-:"+roomCounter+":"+getNumbers(inputLine));
+					bw.flush();
+					
 				}
 			
-				bw.write("Room-:"+roomCounter+":"+inputLine);
 				bw.newLine();
 			}
 			bw.close();
