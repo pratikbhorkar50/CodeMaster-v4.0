6e7363c0672d9fd1f0b288269b8420dbbc2b7136
Jobin George <jobin.gr007@gmail.com>
2021-08-23 12:47:47 +0530

    Solution

--- Problem1/solution.cpp
+++ Problem1/solution.cpp
@@ -1,10 +1,10 @@
 #include<iostream>
+#include <iomanip>
 using namespace std;
 
 int main(int argc, char const *argv[])
 {
 	freopen("Problem1_input.txt","r",stdin);
-	//freopen("inut.txt","r",stdin);
 	freopen("output.txt","w",stdout);
 	int trip;
 	cin>>trip;
@@ -12,18 +12,20 @@ int main(int argc, char const *argv[])
 	{
 		float km, num_vc;
 		cin>>km>>num_vc;
-		double high_hour = 0.0;
-		double mySpeed = 0.0;
+		double high_hour = 0.00000;
+		double mySpeed = 0.00000;
 		for(int j=0; j<num_vc;j++)
 		{
 			double hour;
-			int vc_km, vc_speed;
+			double vc_km, vc_speed;
 			cin>>vc_km>>vc_speed;
 			hour = (km-vc_km)/vc_speed;
 			if(hour > high_hour)
 				high_hour = hour;
 		}
 		mySpeed = km/high_hour;
+		std::cout << std::fixed;
+        std::cout << std::setprecision(6);
 		cout<<"Trip-"<<i+1<<": "<<mySpeed<<endl;
 	}
 
